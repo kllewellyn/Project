fc8a1da0c4e6e52e4014062f5459d182f96a282d
Ryan Brigden <brigdenr@gmail.com>
2015-05-10 21:49:22 -0400

    finished first version of set analyzer

--- Group2/ZetAnalyzer.java
+++ Group2/ZetAnalyzer.java
@@ -20,9 +20,47 @@
 
   - Note that an array passed to findZet can have nulls in some of the elements.
 
+   Represents a card for the game of Set. A card has four attributes: 
+   number: (1, 2, or 3), shape: (oval, squiggle, or diamond), fill (outlined, solid, or striped), 
+   and color (red, green, or blue). Each attribute is represented by an integer code from 1 to 3. 
+
 */
 
 public class ZetAnalyzer
 {
+
+  public ZetAnalyzer()
+  {
+
+  }
+
+  public static int[] findZet(ZetCard[] cards)
+  {
+    for (int j = 0; j < cards.length; j++)
+    {
+      for(int k = 0; k < cards.length; k++)
+      {
+        for (int i = 0; i < cards.length; j++)
+        {
+          if (isZet(cards[i], cards[k], cards[j]))
+            int[] arr = {i, j, k}
+            return arr
+        }
+      }
+    }
+  }
+
+  public static boolean isZet(ZetCard card1, ZetCard card2, ZetCard card3)
+  {
+    if (card1.toString().equals(card2.toString) && card2.toString().equals(card3.toString))
+      return false;
+    boolean colorSet = (card1.getColor() + card2.getColor() + card3.getColor()) % 3 == 0;
+    boolean fillSet = (card1.getFill() + card2.getFill() + card3.getFill()) % 3 == 0;
+    boolean shapeSet = (card1.getShape() + card2.getShape() + card3.getShape()) % 3 == 0;
+    boolean numberSet = (card1.getNumber() + card2.getNumber() + card3.getNumber()) % 3 == 0;
+    return colorSet && fillSet && shapeSet && numberSet;
+  }
+
+
 
 }
